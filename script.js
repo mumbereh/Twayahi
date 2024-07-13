7fc55a2bad666fa742a36844a571503871c1aac7
mumbereh <siirasonh@gmail.com>
2024-07-13 20:24:41 +0300

    changed the name

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger');
-    const navLinks = document.getElementById('nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+}
